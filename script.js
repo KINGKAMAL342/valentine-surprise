ead8b262e3359594c75bb4dd5481dab4b1958c9c
kamal singh <157018642+KINGKAMAL342@users.noreply.github.com>
2026-02-13 22:52:37 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -1,84 +1,75 @@
 let step = 0;
 const text = document.getElementById("text");
 const btn = document.getElementById("btn");
-const music = document.getElementById("music");
+const music = document.getElementById("bgMusic");
 
 const messages = [
   "Hey… I’ve been meaning to tell you something 💌",
-  "Every time you smile, my day gets better 🥺",
-  "So I thought I should be honest with you…",
+  "Every time you smile, my whole day lights up ✨",
+  "So I thought I should be brave today…",
   "Will you be my Valentine? ❤️"
 ];
 
-function typeMessage(msg, i = 0) {
-  if (i < msg.length) {
+function typeMessage(msg, i = 0){
+  if(i < msg.length){
     text.innerHTML += msg.charAt(i);
-    setTimeout(() => typeMessage(msg, i + 1), 50);
+    setTimeout(() => typeMessage(msg, i + 1), 45);
   }
 }
 
-function nextStep() {
+function nextStep(){
   text.innerHTML = "";
   typeMessage(messages[step]);
 
-  if (step === 0) {
+  if(step === 0){
     music.play();
   }
 
-  if (step === 3) {
+  if(step === 3){
     btn.innerHTML = "YES 💖";
     btn.onclick = yesAnswer;
 
     const noBtn = document.createElement("button");
     noBtn.innerHTML = "No 🙈";
+    noBtn.style.marginLeft = "10px";
     noBtn.onclick = noAnswer;
-    document.querySelector(".container").appendChild(noBtn);
+    document.querySelector(".card").appendChild(noBtn);
   }
 
   step++;
 }
 
-function yesAnswer() {
+function yesAnswer(){
   document.getElementById("screen").innerHTML = `
-    <h1>YAYYYY 💕🎉</h1>
-    <p>You just made me the happiest person 🥰</p>
+    <h1>  𝓘 𝓛𝓞𝓥𝓔 𝓨𝓞𝓤 𝓜𝓨 𝓒𝓤𝓣𝓘𝓔𝓟𝓘𝓔 𝓢𝓐𝓝𝓤🤏</h1>
+    <p>You just made my heart very happy 🥰</p>
+    <p>Happy Valentine’s Day 💌</p>
   `;
 }
 
-function noAnswer() {
-  alert("That’s okay 😊 I just wanted to make you smile today 💖");
+function noAnswer(){
+  alert("That’s okay 😊 I just wanted to make you smile 💖");
 }
-/* Auto hearts */
-setInterval(() => {
+
+setInterval(()=>{
   const heart = document.createElement("span");
   heart.innerHTML = "❤️";
   heart.style.left = Math.random() * 100 + "vw";
+  heart.style.fontSize = Math.random() * 10 + 15 + "px";
   document.querySelector(".hearts").appendChild(heart);
+  setTimeout(()=>heart.remove(),6000);
+},350);
 
-  setTimeout(() => heart.remove(), 6000);
-}, 400);
 const images = [
-  "images/pic1.jpg",
-  "images/pic2.jpg",
-  "images/pic3.jpg"
+  "images/img1.jpg",
+  "images/img2.jpg",
+  "images/img3.jpg"
 ];
 
 let index = 0;
 const slide = document.getElementById("slide");
 
-setInterval(() => {
+setInterval(()=>{
   index = (index + 1) % images.length;
   slide.src = images[index];
-}, 2500);
-
-document.addEventListener("DOMContentLoaded", () => {
-  const music = document.getElementById("bgMusic");
-
-  function nextStep() {
-    if (music) {
-      music.play();
-    }
-  }
-
-  window.nextStep = nextStep;
-});
+},2500);
